f2b3e6a0765ddb00603389138a05a6799af6bdaa
PST <tomerps@gmail.com>
2023-11-01 21:44:10 +0000

    adding demo-smartwyte api and pipeline

---
+++ Controllers/healthzController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace smartwyreWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthzController : Controller
+    {
+        [HttpGet("readiness")]
+        public IActionResult Readiness()
+        {           
+            
+            if (isApiReadinessOk())
+            {
+                return Ok(new { status = "ok" });
+            }
+            else
+            {
+                return StatusCode(503, new { status = "API is not ready" });
+            }
+        }
+
+        [HttpGet("liveness")]
+        public IActionResult Liveness()
+        {
+            // Check the liveness condition here
+            if (IsApiHealthy())
+            {
+                return Ok(new { status = "healthy" });
+            }
+            else
+            {
+                return StatusCode(503, new { status = "unhealthy" });
+            }
+        }
+
+        private bool IsApiHealthy()
+        {            
+            // Here's a simple check to ensure the API is always considered healthy:
+            return true;
+        }
+
+        private bool isApiReadinessOk()
+        {
+            // if we have defendence component of API we will check them here and change it based on it.
+            return true;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# weatherapi
+# Introduction 
+Weather API controller api application with one more health controller for the docker heath check.
+
+# Getting Started
+Clone the code locally
+Install docker locallu
+Bulild the docker file for the API and run it locally
+There is full working CI/CD pipeline with the Yamls files to deploy K8s resouces.
+
+
+# Build and Test
+Build in Visual Studio and run the docker build/Run via docker configuraiton mode. 
+
+# Contribute
+Prithvi Tomer
+
+If you want to learn more about creating good readme files then refer the following [guidelines](https://docs.microsoft.com/en-us/azure/devops/repos/git/create-a-readme?view=azure-devops). You can also seek inspiration from the below readme files:
+- [ASP.NET Core](https://github.com/aspnet/Home)
+- [Visual Studio Code](https://github.com/Microsoft/vscode)
+- [Chakra Core](https://github.com/Microsoft/ChakraCore)
